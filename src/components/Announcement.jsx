6a939c0bf3736c3b4b026200e1f6ae14b6c1f291
martin-creator <martinlubowa@outlook.com>
2022-12-29 18:46:49 +0300

    Successfully styled the nav bar

---
+++ src/components/Announcement.jsx
@@ -0,0 +1,22 @@
+import styled from "styled-components"
+
+const Container = styled.div`
+    height: 30px;
+    background-color: teal;
+    color: white;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    font-size: 14px;
+    font-weight: 500;
+
+`
+
+
+export const Announcement = () => {
+  return (
+    <Container>
+         Super Black Friday deal is now! Buy now to save 80% off the original price
+    </Container>
+  )
+}
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,11 @@
 import React from "react";
+import { Announcement } from "../components/Announcement";
 import Navbar from "../components/Navbar";
 
 const Home = () => {
   return (
     <div>
+        <Announcement/>
         <Navbar/>
       <h1>Home Page</h1>
     </div>
