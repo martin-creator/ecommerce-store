a80d6601fce7d29c1f049d0b08b057b1483a7e0b
martin-creator <martinlubowa@outlook.com>
2022-12-29 20:39:57 +0300

    Created basic design for categories

---
+++ src/components/Categories.jsx
@@ -0,0 +1,15 @@
+import styled from "styled-components"
+import { categories } from "../data"
+import { CategoryItem } from "./CategoryItem"
+
+const Container =  styled.div``
+
+export const Categories = () => {
+  return (
+    <Container>
+        {categories.map(item=>(
+            <CategoryItem  item ={item}/>
+        ))}
+    </Container>
+  )
+}
