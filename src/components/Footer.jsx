97917703aded61a7e7d02ff43798cd4e34ba21f6
martin-creator <martinlubowa@outlook.com>
2022-12-30 17:28:01 +0300

    Updated footer sections

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,4 +1,4 @@
-import { Facebook, Instagram, Twitter, Google } from "@material-ui/icons";
+import { Facebook, Instagram, Twitter, Pinterest } from "@material-ui/icons";
 import styled from "styled-components";
 
 const Container = styled.div`
@@ -6,14 +6,60 @@ const Container = styled.div`
 `;
 const Left = styled.div`
   flex: 1;
+  display: flex;
+  flex-direction: column;
+  padding: 20px;
+`;
+
+const Logo = styled.h1`
+  flex: 1;
+`;
+
+const Desc = styled.p`
+  margin: 20px 0px;
+`;
+
+const SocialContainer = styled.div`
+  display: flex;
+`;
+
+const SocialIcon = styled.div`
+  width: 40px;
+  height: 40px;
+  border-radius: 50%;
+  color: white;
+  background-color: #${(props) => props.color};
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  margin-right: 20px;
 `;
 
 const Center = styled.div`
   flex: 1;
+  padding: 20px;
+`;
+
+const Title = styled.h3`
+  margin-bottom: 30px;
+`;
+
+const ListItem = styled.li`
+  width: 50%;
+  margin-bottom: 10px;
+`;
+
+const List = styled.ul`
+  margin: 0;
+  padding: 0;
+  list-style: none;
+  display: flex;
+  flex-wrap: wrap;
 `;
 
 const Right = styled.div`
   flex: 1;
+  padding: 20px;
 `;
 
 const Footer = () => {
@@ -23,28 +69,44 @@ const Footer = () => {
         <Logo>AFRICAN EAGLES</Logo>
         <Desc>
           Eagles fly alone or with their own kind. Life lesson: Associate with
-          people who are at your level, or who can help you to grow. Also, not
-          everyone will share your vision or dream. Find people who think like
-          you so that you can both dream and grow together.
+          people who are at your level, or who can help you to grow.
         </Desc>
         <SocialContainer>
-            <SocialIcon>
-                <Facebook/>
-            </SocialIcon>
-            <SocialIcon>
-                <Instagram/>
-            </SocialIcon>
-            <SocialIcon>
-                <Twitter/> 
-            </SocialIcon>
-            <SocialIcon>
-                <Google/>
-            </SocialIcon>
-
+          <SocialIcon color="3B5999">
+            <Facebook />
+          </SocialIcon>
+          <SocialIcon color="E4405F">
+            <Instagram />
+          </SocialIcon>
+          <SocialIcon color="55ACEE">
+            <Twitter />
+          </SocialIcon>
+          <SocialIcon color="E60023">
+            <Pinterest />
+          </SocialIcon>
         </SocialContainer>
       </Left>
-      <Center></Center>
-      <Right></Right>
+      <Center>
+        <Title>Useful Links</Title>
+        <List>
+          <ListItem>Home</ListItem>
+          <ListItem>Cart</ListItem>
+          <ListItem>Man Fashion</ListItem>
+          <ListItem>Woman Fashion</ListItem>
+          <ListItem>Accessories</ListItem>
+          <ListItem>My Account</ListItem>
+          <ListItem>Order Tracking</ListItem>
+          <ListItem>Wishlist</ListItem>
+          <ListItem>Wishlist</ListItem>
+          <ListItem>Terms</ListItem>
+        </List>
+      </Center>
+      <Right>
+        <Title>Contact</Title>
+        <ContactItem>Kampala, Uganda, 3000</ContactItem>
+        <ContactItem>+256789562634</ContactItem>
+        <ContactItem>eagle@africa.com</ContactItem>
+      </Right>
     </Container>
   );
 };
