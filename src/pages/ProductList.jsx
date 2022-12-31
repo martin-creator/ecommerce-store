8f9ef3b907b29b137bbee5e56e3cb523c7678ee1
martin-creator <martinlubowa@outlook.com>
2022-12-31 10:31:58 +0300

    Created basic description for the product list page

---
+++ src/pages/ProductList.jsx
@@ -0,0 +1,24 @@
+import styled from "styled-components"
+import { Announcement } from "../components/Announcement"
+import Navbar from "../components/Navbar"
+
+const Container = styled.div``
+const Title = styled.h1``
+const FilterContainer = styled.div``
+const Filter = styled.div``
+
+const ProductList = () => {
+  return (
+    <Container>
+        <Navbar/>
+        <Announcement/>
+        <Title>Dresses</Title>
+        <FilterContainer>
+        <Filter>Filter</Filter>
+        <Filter>Filter 2</Filter>
+        </FilterContainer>
+    </Container>
+  )
+}
+
+export default ProductList
