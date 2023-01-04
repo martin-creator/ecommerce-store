298b11373e8d5d292ac918564f33fd1a94628e3f
martin-creator <martinlubowa@outlook.com>
2023-01-04 10:14:37 +0300

    Added env files

--- ecommerce-api/index.js
+++ ecommerce-api/index.js
@@ -1,14 +1,19 @@
 const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
+const dotenv = require("dotenv")
+
+
+dotenv.config()
 
 mongoose.connect(
-    "mongodb+srv://martinlubowa:jesus147852@cluster0.s6launr.mongodb.net/shop?retryWrites=true&w=majority"
+    process.env.MONGO_URL
 ).then(()=>console.log(" DBConnection Successful!"))
  .catch((err)=>{
     console.log(err)
  });
 
-app.listen(5000, ()=>{
+
+app.listen(process.env.PORT || 5000, ()=>{
     console.log("Backend server is running!");
 })
