055ce149b8be6d2e4a8fb3840ea669edca0f7a67
martin-creator <martinlubowa@outlook.com>
2023-01-04 11:50:27 +0300

    Successfully added test get and post api endpoints

--- ecommerce-api/index.js
+++ ecommerce-api/index.js
@@ -7,6 +7,8 @@ const mongoose = require('mongoose');
 
 const dotenv = require('dotenv');
 
+const userRoute = require('./routes/user');
+
 dotenv.config();
 
 /* Database connection */
@@ -18,9 +20,8 @@ mongoose.connect(
     console.log(err);
   });
 
-app.get('/api/test', () => {
-  console.log('Test is successful');
-});
+app.use(express.json());
+app.use('/api/users', userRoute);
 
 /* Port number */
 app.listen(process.env.PORT || 5000, () => {
