a4f9e92b74fca85be63f6555531e2d3603f3d852
martin-creator <martinlubowa@outlook.com>
2023-01-04 09:45:25 +0300

    Created express app and connected to mongodb

--- ecommerce-api/index.js
+++ ecommerce-api/index.js
@@ -1,1 +1,14 @@
-console.log("hello")
+const express = require("express");
+const app = express();
+const mongoose = require("mongoose");
+
+mongoose.connect(
+    "mongodb+srv://martinlubowa:jesus147852@cluster0.s6launr.mongodb.net/shop?retryWrites=true&w=majority"
+).then(()=>console.log(" DBConnection Successful!"))
+ .catch((err)=>{
+    console.log(err)
+ });
+
+app.listen(5000, ()=>{
+    console.log("Backend server is running!");
+})
