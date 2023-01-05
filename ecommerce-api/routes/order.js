0bd5f33440b1e16e9de720c9bcc4828d98e4d2d3
martin-creator <martinlubowa@outlook.com>
2023-01-05 13:04:34 +0300

    Created api endpoint to update order

--- ecommerce-api/routes/order.js
+++ ecommerce-api/routes/order.js
@@ -15,17 +15,17 @@ router.post('/', verifyToken, async (req, res) => {
   }
 });
 
-// // Update Order
-// router.put('/:id', verifyTokenAndAdmin, async (req, res) => {
-//   try {
-//     const updatedOrder = await Order.findByIdAndUpdate(req.params.id, {
-//       $set: req.body,
-//     }, { new: true });
-//     res.status(200).json(updatedOrder);
-//   } catch (err) {
-//     res.status(500).json(err);
-//   }
-// });
+// Update Order
+router.put('/:id', verifyTokenAndAdmin, async (req, res) => {
+  try {
+    const updatedOrder = await Order.findByIdAndUpdate(req.params.id, {
+      $set: req.body,
+    }, { new: true });
+    res.status(200).json(updatedOrder);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
 
 // // Delete Order
 // router.delete('/:id', verifyTokenAndAdmin, async (req, res) => {
