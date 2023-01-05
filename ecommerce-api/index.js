02d6a49854a869ecb9ca78e7cd2d4c14c284f4d6
martin-creator <martinlubowa@outlook.com>
2023-01-05 12:20:43 +0300

    Created api end point to create product

--- ecommerce-api/index.js
+++ ecommerce-api/index.js
@@ -9,6 +9,7 @@ const dotenv = require('dotenv');
 
 const userRoute = require('./routes/user');
 const authRoute = require('./routes/auth');
+const productRoute = require('./routes/product');
 
 dotenv.config();
 
@@ -24,6 +25,7 @@ mongoose.connect(
 app.use(express.json());
 app.use('/api/auth', authRoute);
 app.use('/api/users', userRoute);
+app.use('/api/products', productRoute);
 
 /* Port number */
 app.listen(process.env.PORT || 5000, () => {
