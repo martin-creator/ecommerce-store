4b0b287218e037e14e0f8015f79a3de9ef42a40f
martin-creator <martinlubowa@outlook.com>
2023-01-05 09:16:29 +0300

    Created delete  and get user endpoints

--- ecommerce-api/routes/user.js
+++ ecommerce-api/routes/user.js
@@ -2,8 +2,9 @@ const router = require('express').Router();
 const CryptoJS = require('crypto-js');
 const User = require('../models/User');
 
-const { verifyToken, verifyTokenAndAuthorization } = require('./verifyToken');
+const { verifyToken, verifyTokenAndAuthorization, verifyTokenAndAdmin } = require('./verifyToken');
 
+// Update User
 router.put('/:id', verifyTokenAndAuthorization, async (req, res) => {
   if (req.body.password) {
     req.body.password = CryptoJS.AES.encrypt(req.body.password, process.env.PASS_SEC).toString();
@@ -18,4 +19,28 @@ router.put('/:id', verifyTokenAndAuthorization, async (req, res) => {
     res.status(500).json(err);
   }
 });
+
+// Delete User
+router.delete('/:id', verifyTokenAndAuthorization, async (req, res) => {
+  try {
+    await User.findByIdAndDelete(req.params.id);
+    res.status(200).json('User has been successfully deleted');
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+// Get User
+// eslint-disable-next-line consistent-return
+router.get('/find/:id', verifyTokenAndAdmin, async (req, res) => {
+  try {
+    const user = await User.findById(req.params.id);
+    const { password, ...others } = user._doc;
+
+    return res.status(200).json({ others }); // separates password from being sent  in res object
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
 module.exports = router;
