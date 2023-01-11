8ffc5620307efab174234456cad677710f9ec0d4
martin-creator <martinlubowa@outlook.com>
2023-01-11 17:03:59 +0300

    Created home page

--- ecommerce-admin/src/App.js
+++ ecommerce-admin/src/App.js
@@ -1,6 +1,7 @@
 import  Sidebar from './components/sidebar/Sidebar';
 import Topbar from './components/topbar/Topbar';
-import "./App.css"
+import "./App.css";
+import Home from './pages/home/Home';
 
 function App() {
   return (
@@ -8,7 +9,7 @@ function App() {
       <Topbar />
       <div className='container'>
         <Sidebar />
-        otherpages
+        <Home />
       </div>
     </div>
   );
