a1fd068fa90a5046c4169f16a99351eb116a782b
martin-creator <martinlubowa@outlook.com>
2023-01-11 19:05:42 +0300

    Successfully created widgets

--- ecommerce-admin/src/pages/home/Home.jsx
+++ ecommerce-admin/src/pages/home/Home.jsx
@@ -2,6 +2,8 @@ import FeaturedInfo from '../../components/featureInfo/FeatureInfo';
 import './home.css';
 import { userData } from '../../dummyData';
 import Chart from '../../components/chart/Chart';
+import WidgetSm from '../../components/widgetSm/WidgetSm';
+import WidgetLg from '../../components/widgetLg/WidgetLg';
 
 export const Home = () => {
   return (
@@ -9,7 +11,8 @@ export const Home = () => {
         <FeaturedInfo />
         <Chart data={userData} title="User Analytics" grid dataKey="Active User"/>
         <div className="homeWidgets">
-            
+            <WidgetSm />
+            <WidgetLg />
         </div>
     </div>
   )
