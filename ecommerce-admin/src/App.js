1f26c9bcb5888a70cbee4b95ce983a56dfacb90c
martin-creator <martinlubowa@outlook.com>
2023-01-12 13:57:14 +0300

    Created datagrid product list

--- ecommerce-admin/src/App.js
+++ ecommerce-admin/src/App.js
@@ -5,26 +5,40 @@ import { Home } from './pages/home/Home';
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import UserList from './pages/userList/UserList';
 import User from './pages/user/User';
+import NewUser from './pages/NewUser/NewUser';
+import ProductList from './pages/productList/ProductList';
 
 function App() {
   return (
     <Router>
-      <Topbar />
-      <div className='container'>
-        <Sidebar />
-        <Switch>
-          <Route exact path='/'>
-            <Home />
-          </Route>
-          <Route path='/users'>
-            <UserList />
-          </Route>
-          <Route path='/users/:userId'>
-            <User/>
-          </Route>
-        </Switch>
-      </div>
-    </Router>
+    <Topbar />
+    <div className="container">
+      <Sidebar />
+      <Switch>
+        <Route exact path="/">
+          <Home />
+        </Route>
+        <Route path="/users">
+          <UserList />
+        </Route>
+        <Route path="/user/:userId">
+          <User />
+        </Route>
+        <Route path="/newUser">
+          <NewUser />
+        </Route>
+        <Route path="/products">
+          <ProductList />
+        </Route>
+        <Route path="/product/:productId">
+          <ProductList />
+        </Route>
+        <Route path="/newproduct">
+          <ProductList />
+        </Route>
+      </Switch>
+    </div>
+  </Router>
   );
 }
 
