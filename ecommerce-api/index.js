34899ada19567d5cdb58cb21ca19b20499659e46
martin-creator <martinlubowa@outlook.com>
2023-01-23 14:46:23 +0300

    Created orders routes

--- ecommerce-api/index.js
+++ ecommerce-api/index.js
@@ -10,6 +10,8 @@ const dotenv = require('dotenv');
 const userRoute = require('./routes/user');
 const authRoute = require('./routes/auth');
 const productRoute = require('./routes/product');
+const cartRoute = require('./routes/cart');
+const orderRoute = require('./routes/order');
 
 dotenv.config();
 
@@ -26,7 +28,8 @@ app.use(express.json());
 app.use('/api/auth', authRoute);
 app.use('/api/users', userRoute);
 app.use('/api/products', productRoute);
-
+app.use('/api/carts', cartRoute);
+app.use('/api/orders', orderRoute);
 /* Port number */
 app.listen(process.env.PORT || 5000, () => {
   console.log('Backend server is running!');
